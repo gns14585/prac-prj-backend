943ccc305fa9ab853a49014a4d2be91c3dd6632e
LeeJungHoon <gns14585@naver.com>
2023-11-09 21:38:25 +0900

    최초 설정 및 BoardController post /api/board/add 작성

---
+++ src/main/java/com/example/pracprj1backend/PracPrj1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracprj1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracPrj1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracPrj1BackendApplication.class, args);
+    }
+
+}
