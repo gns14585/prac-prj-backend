c931149ad09ddd59cc532b0d5917ebbe923ee98e
이정훈 <gns14585@naver.com>
2023-11-20 23:04:43 +0900

    현재 페이지, 처음 페이지, 마지막 페이지 추가

--- src/main/java/com/example/pracprj1backend/service/BoardSerivce.java
+++ src/main/java/com/example/pracprj1backend/service/BoardSerivce.java
@@ -9,9 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -47,22 +45,27 @@ public class BoardSerivce {
         Map<String, Object> pageInfo = new HashMap<>();
 
         int countAll = mapper.countAll(); // 총 게시물 수
-        int lastPagenumber = (countAll - 1) / 10 + 1; // 최종 마지막 페이지 번호
+        int lastPageNumber = (countAll - 1) / 10 + 1; // 최종 마지막 페이지 번호
         int startPageNumber = (page - 1) / 10 * 10 + 1; // 1~10 중 1페이지 시작 페이지 번호
         int endPageNumber = startPageNumber + 9; // 1~10 중 10페이지 마지막 페이지 번호
-        endPageNumber = Math.min(endPageNumber, lastPagenumber);
+        endPageNumber = Math.min(endPageNumber, lastPageNumber);
         int prevPageNumber = startPageNumber - 10;
         int nextPageNumber = endPageNumber + 1;
+        int firstPageNumber = 1;
 
+        pageInfo.put("currentPageNumber", page); // 현재 페이지
         pageInfo.put("startPageNumber", startPageNumber);
         pageInfo.put("endPageNumber", endPageNumber);
 
         if (prevPageNumber > 0) { // 페이지번호 0 보다 클 경우에만 화살표 버튼 보여지게
             pageInfo.put("prevPageNumber", prevPageNumber); // 왼쪽버튼 눌렀을때 10페이지 전으로 이동
+            pageInfo.put("firstPageNumber", firstPageNumber); // 처음 페이지로 이동
         }
-        if (nextPageNumber <= lastPagenumber) { // lastPageNumber 보다 작을 경우에만 보여지게
+        if (nextPageNumber <= lastPageNumber) { // lastPageNumber 보다 작을 경우에만 보여지게
             pageInfo.put("nextPageNumber", nextPageNumber);  // 마지막 페이지에선 오른쪽으로 가는 버튼 보여지지않음
+            pageInfo.put("lastPageNumber", lastPageNumber);  // 최종 마지막 페이지로 이동
         }
+
 
         int from = (page - 1) * 10;
         map.put("boardList", mapper.selectAll(from));
