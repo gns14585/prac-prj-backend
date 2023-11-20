d3e1bb724825ec64275497d92572910709eedce9
이정훈 <gns14585@naver.com>
2023-11-20 22:37:20 +0900

    BoardService 오타 수정
    pageInfo.put("lastPageNumber", lastPageNumber); ->
    pageInfo.put("endPageNumber", endPageNumber);

--- src/main/java/com/example/pracprj1backend/service/BoardSerivce.java
+++ src/main/java/com/example/pracprj1backend/service/BoardSerivce.java
@@ -53,7 +53,7 @@ public class BoardSerivce {
         endPageNumber = Math.min(endPageNumber, lastPagenumber);
 
         pageInfo.put("startPageNumber", startPageNumber);
-        pageInfo.put("lastPageNumber", lastPagenumber);
+        pageInfo.put("endPageNumber", endPageNumber);
 
         int from = (page - 1) * 10;
         map.put("boardList", mapper.selectAll(from));
