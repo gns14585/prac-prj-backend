8cba25f8027b119c87801417f1a99508292a0a2a
LeeJungHoon <gns14585@naver.com>
2023-11-20 17:14:39 +0900

    좋아요 응답 {like, countLike} 기능

--- src/main/java/com/example/pracprj1backend/mapper/LikeMapper.java
+++ src/main/java/com/example/pracprj1backend/mapper/LikeMapper.java
@@ -4,6 +4,7 @@ import com.example.pracprj1backend.domain.Like;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface LikeMapper {
@@ -20,4 +21,10 @@ public interface LikeMapper {
             VALUES (#{boardId}, #{memberId})
             """)
     int insert(Like like);
+
+    @Select("""
+            SELECT COUNT(id) FROM boardlike
+            WHERE boardId = #{boardId}
+            """)
+    int countByBoardId(String boardId);
 }
